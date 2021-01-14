c12a965fd5fed2dc040f3459d5677824dc04931a
ahmatrizal <ahmat.rizal@outlook.com>
2021-01-14 23:45:03 +0700

    Membuat Component Navbar, ListCategory dan Hasil, dan install Json Placeholder untuk data dummy backend

---
+++ src/components/ListCategory.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Col } from 'react-bootstrap'
+
+export default class ListCategory extends Component {
+    render() {
+        return (
+            <Col md={2} mt='2'>
+            <h4>Category</h4>
+            <hr />
+            </Col>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Row, Container } from 'react-bootstrap'
+
+
+import Navbar from './components/NavbarComponent'
+import Category from './components/ListCategory'
+import Hasil from './components/Hasil'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+    <div className="mt-3">
+      <Container fluid>
+      <Row>
+        <Category />
+        <Col>
+          <h4>Daftar Product</h4>
+          <hr />
+        </Col>
+        <Hasil />
+      </Row>   
+    </Container>
+    </div>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
